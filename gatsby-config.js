0ff20aeff40a4be205c23c839da3f32703981370
Eric Hodges <ehodges2004@gmail.com>
2020-03-14 22:13:56 -0700

    Chore: Updated project name in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `Gatsby-React-hooks-image-slider`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#000`,
